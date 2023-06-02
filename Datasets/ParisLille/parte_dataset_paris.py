04fda5db6936bfac3b2ed1c9d6e97346c23df6f1
Sebastian Fernandez <sebasfernandezgar@gmail.com>
2023-06-02 09:53:25 +0100

    feat: :sparkles: mejoras ya arreglos en procesa paris, ya no hay huecazos

---
+++ Datasets/ParisLille/parte_dataset_paris.py
@@ -0,0 +1,244 @@
+#import argparse
+#import os
+from plyfile import PlyData, PlyElement
+import numpy as np
+from sklearn.decomposition import PCA
+#import pptk
+
+import os
+import numpy as np
+import argparse
+#import laspy
+import math
+from tqdm import tqdm
+import random 
+import shutil
+import numpy as np
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+
+import open3d as o3d
+#import pyntcloud
+import pandas as pd
+#from pyntcloud import Voxelize
+from scipy.spatial import cKDTree
+import time
+
+parser = argparse.ArgumentParser()
+
+"""
+parser.add_argument('--segment_point_size', type=int, default=131072, help='resample points number') #Choose here number of point resample
+parser.add_argument('--dataset_type', type=str, choices = ('train', 'test'), default='train', help='if train, dataset just splited, downsampled, normalized, if test same, but also gives numbers to reconstruct and paint dataset later')
+parser.add_argument('--output_dataset_folder', type=str, default=r'nuevaparticion/test/', help='train datasetfolder') #las_test
+"""
+
+parser.add_argument('--original_las', type=str, default=r'C:/Users/sfernandez/nueva_etapa/github2/LidarSegmentationPytorch/Datasets/ParisLille/training_10_classes', help='train datasetfolder') #las_test
+parser.add_argument('--dataset_type', type=str, choices = ('train', 'test'), default='train', help='if train, dataset just splited, downsampled, normalized, if test same, but also gives numbers to reconstruct and paint dataset later')
+opt = parser.parse_args()
+
+
+#opt.original_las
+
+def split_dataset(output_folder):
+    print("Procesando nubes...")
+
+    # Ruta de la carpeta para guardar los segmentos
+    #output_folder = opt.output_dataset_folder
+
+    # Crear la carpeta si no existe
+    #os.makedirs(output_folder, exist_ok=True)
+
+    for filename in tqdm(os.listdir(opt.original_las), desc ="Conjunto de Todas las nubes:"):
+        f = os.path.join(opt.original_las, filename)
+        if os.path.isfile(f):
+
+            #print(f)
+
+            plydata = PlyData.read(f) #print(plydata)
+
+            x = plydata["vertex"].data["x"].astype(np.float32)
+            y = plydata["vertex"].data["y"].astype(np.float32)
+            z = plydata["vertex"].data["z"].astype(np.float32)
+
+            point_data = np.stack([x, y, z], axis=0)
+
+            vertex_data = plydata["vertex"].data
+
+            print(vertex_data)
+
+            nombres_campos = vertex_data.dtype.names
+            print(nombres_campos)
+
+            """ TEST
+            #TEST - ('x', 'y', 'z', 'x_origin', 'y_origin', 'z_origin', 'GPS_time', 'reflectance')
+            x_origin = plydata["vertex"].data["x_origin"].astype(np.float32)
+            y_origin = plydata["vertex"].data["y_origin"].astype(np.float32)
+            z_origin = plydata["vertex"].data["z_origin"].astype(np.float32)
+
+
+            nueva_x = x-x_origin
+
+            nueva_y = y-y_origin
+
+            nueva_z = z-z_origin
+            reflectance = plydata["vertex"].data["reflectance"].astype(np.float32)
+            xyzic = np.stack([nueva_x, nueva_y, nueva_z, reflectance], axis=0).transpose((1, 0))
+
+            """
+
+            """
+            print(max(x)-min(x))
+            print("----")
+            print(max(y)-min(y))
+            print("----")
+            print(max(z)-min(z))
+            print("----")
+            """
+            
+            
+            #ESTO SI HAY QUE RESTAR 1 A LAS CLASESS
+            #adapta_clases = las.classification
+            #adapta_clases = np.subtract(adapta_clases, 1)
+
+            reflectance = plydata["vertex"].data["reflectance"].astype(np.float32)
+
+            label = plydata["vertex"].data["class"].astype(np.float32) #label solo si es train
+            
+            #print(plydata["vertex"].data)
+
+            #last_column = [row[-1] for row in plydata["vertex"].data]
+            #unique_elements = set(last_column)
+            #print(unique_elements)
+            #true_label = plydata["vertex"].data["label"].astype(np.float32) #label solo si es train
+            #reflectance = label = plydata["vertex"].data["reflectance"].astype(np.float32) #label solo si es train
+
+            #xyz = np.array([x,y,z]).T
+            
+            xyzic = np.stack([x, y, z, reflectance, label], axis=0).transpose((1, 0))
+            #print(xyzic.shape)
+
+            print(xyzic)
+            data = xyzic
+
+
+            base_filename = os.path.basename(f)
+            # Create a pandas DataFrame from the data for easier manipulation
+            df = pd.DataFrame(data, columns=['x', 'y', 'z', 'reflectance', 'class'])
+
+            # Define grid size
+            grid_size = 20.0
+
+            # Calculate grid indices
+            df['grid_x'] = (df['x'] / grid_size).apply(math.floor)
+            df['grid_y'] = (df['y'] / grid_size).apply(math.floor)
+
+            # Group by grid indices
+            grouped = df.groupby(['grid_x', 'grid_y'])
+
+            # Create 'segments' directory if it doesn't exist
+            os.makedirs('segments', exist_ok=True)
+
+            # Loop over each group
+            for i, (name, group) in enumerate(grouped):
+                # Drop grid indices columns from group
+                group = group.drop(['grid_x', 'grid_y'], axis=1)
+
+                # Convert group back to numpy array
+                group_array = group.to_numpy()
+
+                # Save to .npy file, appending group name to filename
+                output_filename = os.path.join('segments', f'{base_filename}_{i}.npy')
+                if len(group_array) > 1:
+                    #print(output_filename)
+                    np.save(output_filename, group_array)
+            #time.sleep(10)
+
+
+            """
+            ###################
+
+            x_min = np.min(xyzic[:, 0])
+            x_max = np.max(xyzic[:, 0])
+            y_min = np.min(xyzic[:, 1])
+            y_max = np.max(xyzic[:, 1])
+
+            divide_x = np.ceil((x_max - x_min) / 20).astype(int)
+            divide_y = np.ceil((y_max - y_min) / 20).astype(int)
+
+            count = 0
+            for i in range(divide_x):
+                for j in range(divide_y):
+                    # Definir límites de segmento
+                    x_min_segment = x_min + i * 20
+                    x_max_segment = x_min + (i + 1) * 20
+                    y_min_segment = y_min + j * 20
+                    y_max_segment = y_min + (j + 1) * 20
+
+                    # Filtrar puntos dentro del segmento
+                    segment_points = xyzic[
+                        (xyzic[:, 0] >= x_min_segment)
+                        & (xyzic[:, 0] < x_max_segment)
+                        & (xyzic[:, 1] >= y_min_segment)
+                        & (xyzic[:, 1] < y_max_segment)
+                    ]
+                    count += 1
+
+                    filename_seg = os.path.splitext(filename)[0]+f"_{str(count).zfill(3)}"
+                    #filename = f"segment_{i}_{j}.npy"
+
+                    filepath = os.path.join(output_folder, filename_seg)
+                    if len(segment_points)>1:
+                        np.save(filepath, segment_points)
+            """
+
+def train_valid_split(source_folder):
+
+    # Set the percentage of files to be moved to the validation folder
+    validation_split = 0.2
+
+    #train_dataset_folder = opt.output_dataset_folder+'train/'
+    #source_folder = opt.output_dataset_folder #train_dataset_folder
+    
+    # Create the destination folders if they don't already exist
+    #train_folder = train_dataset_folder + "train"
+    #validation_folder = train_dataset_folder + "validation"
+
+    train_folder = source_folder + "/train"
+    validation_folder = source_folder + "/validation"
+
+    os.makedirs(train_folder, exist_ok=True)
+    os.makedirs(validation_folder, exist_ok=True)
+
+    # Get the list of .npy files in the source folder
+    file_list = [f for f in os.listdir(source_folder) if f.endswith('.npy')]
+
+    # Calculate the number of files to be moved to the validation folder
+    num_val_files = int(len(file_list) * validation_split)
+
+    # Randomly select the files to be moved to the validation folder
+    val_files = random.sample(file_list, num_val_files)
+
+    # Move the files to the train and validation folders
+    for file_name in file_list:
+        if file_name in val_files:
+            dest_folder = validation_folder
+        else:
+            dest_folder = train_folder
+        shutil.move(os.path.join(source_folder, file_name), os.path.join(dest_folder, file_name))
+
+if __name__ == "__main__":
+
+    if(opt.dataset_type == 'train'):
+        outputfolder = r'train/'
+        #Create the destination folders if they don't already exist
+        os.makedirs(outputfolder, exist_ok=True)
+
+        split_dataset(outputfolder)
+        train_valid_split(outputfolder)
+
+    if opt.dataset_type == 'test':
+        outputfolder = r'test/'
+        #Create the destination folders if they don't already exist
+        os.makedirs(outputfolder, exist_ok=True)
+        split_dataset(outputfolder)
+
+    print("Finished Spliting, Downscaling and Normalized Dataset. You can find the results at: ", outputfolder)
---
+++ Datasets/ParisLille/procesa_dataset_paris.py
@@ -0,0 +1,180 @@
+import numpy as np
+import os
+import time 
+import random
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--split', type=str, choices = ('train', 'validation', 'test'), required=True, help='if train, dataset is splited, downsampled, normalized, if test same, but also gives numbers to reconstruct and paint dataset later')
+parser.add_argument('--range', type=int, choices = (-1, 0), required=True, help='normaliza entre 0 y 1 o -1,1')
+parser.add_argument('--puntos_finales', type=int, choices = (1024, 2048, 4096, 16384, 32768, 50000), required=True, help='puntos que tendrá cada fichero final')
+opt = parser.parse_args()
+
+def normalize_dataset(data):
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
+    r = data[:, 3]
+    c = data[:, 4]
+
+    x_normalized1 = (x-min(x))
+    x_normalized = x_normalized1/max(x_normalized1)
+
+    y_normalized1 = (y-min(y))
+    y_normalized = y_normalized1/max(y_normalized1)
+
+    z_normalized1 = (z-min(z))
+    z_normalized = z_normalized1/100
+
+    return return_normalized(x_normalized1, y_normalized1, x_normalized, y_normalized, z_normalized, r, c)
+
+def normalize_dataset_1_1(data):
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
+    r = data[:, 3]
+    c = data[:, 4]
+
+    x_normalized1 = (x - min(x))
+    x_normalized2 = x_normalized1 / max(x_normalized1)
+    x_normalized = 2 * x_normalized2 - 1
+
+    y_normalized1 = (y - min(y))
+    y_normalized2 = y_normalized1 / max(y_normalized1)
+    y_normalized = 2 * y_normalized2 - 1
+
+    z_normalized1 = (z - min(z))
+    z_normalized2 = z_normalized1 / 100
+    z_normalized = 2 * z_normalized2 - 1
+
+    return return_normalized(x_normalized1, y_normalized1, x_normalized, y_normalized, z_normalized, r, c)
+
+def return_normalized(x_normalized1, y_normalized1, x_normalized, y_normalized, z_normalized, r, c):
+    if(opt.split == 'test'):
+        
+        max_normalized_x_all_array = np.full(len(c), max(x_normalized1))
+        max_normalized_y_all_array = np.full(len(c), max(y_normalized1))
+        min_x_all_array = np.full(len(c), min(x))
+        min_y_all_array = np.full(len(c), min(y))
+        min_z_all_array = np.full(len(c), min(z))
+
+        xyzrc_normalized = np.array([x_normalized,y_normalized,z_normalized,r,c,
+                                    max_normalized_x_all_array, max_normalized_y_all_array, 
+                                    min_x_all_array, min_y_all_array, min_z_all_array]).T #max(x_normalized), max(y_normalized), min(x), min(y), min(z)]).T
+        
+        #np.save(os.path.join(test_dataset_folder, filename_seg), xyzrc_normalized)
+        return xyzrc_normalized
+    
+    if(opt.split == 'train' or opt.split == 'validation'):
+
+        xyzrc_normalized = np.array([x_normalized,y_normalized,z_normalized,r,c]).T #max(x_normalized), max(y_normalized), min(x), min(y), min(z)]).T
+        return xyzrc_normalized
+        #np.save(os.path.join(train_dataset_folder, filename_seg), xyzrc_normalized)
+
+
+
+if opt.split == 'train':
+    print("Procesando Train")
+
+    las_dir = 'train_a20metros/train/' #origen
+
+    #Para guardar
+    if(opt.range == 0):
+        save_dir = 'train/train/procesados' + str(opt.puntos_finales) + '-' + str(opt.range) + '_' + str(opt.range+1) + '/'
+    if(opt.range == -1):
+        save_dir = 'train/train/procesados' + str(opt.puntos_finales) + str(opt.range) + '_' + str(opt.range+2) + '/'
+    
+if opt.split == 'validation':
+    print("Procesando Validation")
+
+    las_dir = 'train_a20metros/validation/'
+    if(opt.range == 0):
+        save_dir = 'train/validation/procesados' + str(opt.puntos_finales) + '-' + str(opt.range) + '_' + str(opt.range+1) + '/'
+    if(opt.range == -1):
+        save_dir = 'train/validation/procesados' + str(opt.puntos_finales)  + str(opt.range) + '_' + str(opt.range+2) + '/'
+
+if opt.split == 'test':
+    print("Procesando Test")
+
+    #las_dir = 'test/solopartidos/' #origen
+    las_dir = 'train_a20metros/faketest/' #origen
+    las_dir = 'segments/' #origen
+    #las_dir = 'train/train/solopartidos/' #origen
+    if(opt.range == 0):
+        save_dir = 'testFAKE/procesados' + str(opt.puntos_finales) + '-' + str(opt.range) + '_' + str(opt.range+1) + '/'
+    if(opt.range == -1):
+        save_dir = 'testFAKE/procesados' + str(opt.puntos_finales) + str(opt.range) + '_' + str(opt.range+2) + '/'
+
+datos_fragmentos = []
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+for filename in tqdm(os.listdir(las_dir)):
+    if filename.endswith('.npy'):
+        #data = np.load(las_dir+filename)
+
+        nubetemp = np.load(las_dir+filename)
+        #print(nubetemp)
+        #time.sleep(10)
+
+        npoints = opt.puntos_finales
+        choice = np.random.choice(nubetemp.shape[0], npoints, replace=True)
+        randomNube = nubetemp[choice, :]#, c[choice]
+
+        #####
+
+        x = nubetemp[:, 0]
+        y = nubetemp[:, 1]
+        z = nubetemp[:, 2]
+
+        voxel_size = 1  # Adjust this value based on your requirements
+        voxel_indices = ((x / voxel_size).astype(int), (y / voxel_size).astype(int), (z / voxel_size).astype(int))
+
+        voxel_dict = {}
+        for i in range(nubetemp.shape[0]):
+            voxel_index = voxel_indices[0][i], voxel_indices[1][i], voxel_indices[2][i]
+            if voxel_index in voxel_dict:
+                voxel_dict[voxel_index].append(nubetemp[i])
+            else:
+                voxel_dict[voxel_index] = [nubetemp[i]]
+
+        fixed_num_points = opt.puntos_finales #16384  # Adjust this value based on your requirements
+        num_points_per_voxel = int(np.ceil(fixed_num_points / len(voxel_dict)))
+
+        downsampled_points = []
+        voxel_indices = set(zip(*voxel_indices))
+
+        num_points_per_voxel = int(np.ceil(fixed_num_points / len(voxel_indices)))
+
+        for voxel_index in voxel_indices:
+            voxel_points = voxel_dict.get(voxel_index, [])
+            if len(voxel_points) <= num_points_per_voxel:
+                downsampled_points.extend(voxel_points)
+            else:
+                random_indices = random.sample(range(len(voxel_points)), num_points_per_voxel)
+                downsampled_points.extend(np.array(voxel_points)[random_indices])
+
+        downsampled_points = np.array(downsampled_points[:fixed_num_points])
+        
+
+        #puntos_normalizados = normalize_dataset_1_1(downsampled_points)
+
+        #np.save(save_dir + filename, puntos_normalizados)
+
+     
+        if len(downsampled_points)>1: #que haya mas de 16 puntos arbitrariamente
+            
+            if(opt.range == 0):
+                puntos_normalizados = normalize_dataset(downsampled_points)
+            if(opt.range == -1):
+                puntos_normalizados = normalize_dataset_1_1(downsampled_points)
+
+            #Guardar random para duplicar puntos 
+            indices = np.random.choice(puntos_normalizados.shape[0], size=opt.puntos_finales, replace=True)
+            resized = puntos_normalizados = puntos_normalizados[indices]
+            #resized = puntos_normalizados
+            np.save(save_dir + filename, resized)
+            
+    
